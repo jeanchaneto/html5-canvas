f1f3be335d8c4987eb6629bc4e120828bfc9f596
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-08 21:42:51 +0200

    Mouse drawing + fix canvas setup

--- script.js
+++ script.js
@@ -5,19 +5,39 @@ const canvas = document.getElementById("canvas1");
 //Return reference object to draw in 2d
 const ctx = canvas.getContext("2d");
 console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 //prevent stretching of drawn items when resizing window
 canvas.addEventListener("resize", () => {
-  canvas.width = windows.innerWidth;
+  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
 
-//Drawing circle with .arc()
-ctx.fillStyle = "green";
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-//Need to tell js where to start when draing arcs/circles
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 2);
-ctx.stroke();
-ctx.fill();
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+canvas.addEventListener("click", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+
+const drawCircle = () => {
+  //Drawing circle with .arc()
+  ctx.fillStyle = "teal";
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 1;
+  //Need to tell js where to start when drawing arcs/circles
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 5, 0, Math.PI * 2);
+  ctx.fill();
+};
